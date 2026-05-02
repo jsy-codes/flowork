08cbbbe500b6eb5279b47ec47e120364c9a602b0
jsy-codes <port9746@gmail.com>
2026-05-02 23:46:31 +0900

    chore: initialize project environment with docker-compose, mysql, redis setup

---
+++ src/main/java/com/flowork/flowork/FloworkApplication.java
@@ -0,0 +1,15 @@
+package com.flowork.flowork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FloworkApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FloworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flowork/flowork/FloworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowork.flowork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
